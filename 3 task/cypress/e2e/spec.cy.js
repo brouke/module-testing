f249b78162f1c4a350c350503b53c36149f8d2c4
brouke <typevlone@gmail.com>
2024-11-17 21:28:18 +0400

    fix но сайт не отвечает (401)

--- 3 task/cypress/e2e/spec.cy.js
+++ 3 task/cypress/e2e/spec.cy.js
@@ -3,19 +3,19 @@ describe("example to-do app", () => {
     cy.visit("https://www.saucedemo.com");
   });
 
-  it("success auth", () => {
-    cy.get('[data-test="username"]').type('standard_user');
-    cy.get('[data-test="password"]').type('secret_sauce');
-    cy.get('[data-test="login-button"]').click();
-
-    cy.url().should("eq", "https://www.saucedemo.com/inventory.html");
-  });
   it("error auth", () => {
 
     cy.get('[data-test="username"]').type('qwe');
     cy.get('[data-test="password"]').type('qwe');
     cy.get('[data-test="login-button"]').click();
 
+  });
+  it("success auth", () => {
+    cy.get('[data-test="username"]').type('standard_user');
+    cy.get('[data-test="password"]').type('secret_sauce');
+    cy.get('[data-test="login-button"]').click();
+
+    cy.url().should("eq", "https://www.saucedemo.com/inventory.html");
   });
 
   it("sorting asc price", () => {
